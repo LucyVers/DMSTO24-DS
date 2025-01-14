c0f21d23ee5563d8273743981670e019176e3b77
LucyVers <116066287+LucyVers@users.noreply.github.com>
2025-01-14 10:34:04 +0100

    Lade till multiplication_table funktionen

--- inlupp/uppgift-6/uppgift_6.py
+++ inlupp/uppgift-6/uppgift_6.py
@@ -1,6 +1,5 @@
 # Uppgift 6
 # Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
-
 def multiplication_table(n: int, limit: int) -> list:
     """
     Skapar multiplikationstabellen för n upp till limit.
