feb9702f5223f0655ba3dbfb55a41203a7a730fb
LucyVers <116066287+LucyVers@users.noreply.github.com>
2025-01-15 10:37:33 +0100

    Uppdaterade sum_list funktionen

--- inlupp/uppgift-2/uppgift_2.py
+++ inlupp/uppgift-2/uppgift_2.py
@@ -1,6 +1,3 @@
-# Uppgift 2
-# Skapa en funktion sum_list(numbers) som returnerar summan av alla siffror i listan.
-
 def sum_list(numbers: list) -> int:
     """
     Jag returnerar summan av alla siffror i listan.
